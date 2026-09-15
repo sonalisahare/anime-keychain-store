19419ceeb18fe140bcdd87fcbfaa819f90d33ac5
sonalisahare <sonalisahare31@gmail.com>
2026-09-15 14:52:42 +0530

    Serve product media files

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -15,17 +15,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
+from django.conf import settings
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('product.urls')),
+    path('', include('product.urls')),
 ]
 
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(
-    settings.MEDIA_URL,
-    document_root=settings.MEDIA_ROOT
-)
+urlpatterns += [
+    path(
+        'media/<path:path>',
+        serve,
+        {'document_root': settings.MEDIA_ROOT}
+    ),
+]
